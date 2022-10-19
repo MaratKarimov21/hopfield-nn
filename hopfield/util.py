3cd34f093e1af91c5ba4c22e6b431221c7019f27
MaratKarimov21 <marat.karimov@stackers.team>
2022-10-19 22:29:48 +0300

    hopfield

---
+++ hopfield/util.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: cp1251 -*-
+#
+# Author: Yaroslav Pogrebnyak (yyyaroslav@gmail.com)
+# http://bitsofmind.wordpress.com
+#
+
+# Символ для значения нейрона
+dict = { -1 : '-', 1 : '@' }
+def charfor(x):
+    return dict[x]
+
+#
+# Печатает образ в виде ASCII-графики
+#
+def printshape(obraz, size):
+    i = 0
+    for o in obraz:
+        print(charfor(o), end="")
+        i+=1
+        if i % size == 0:
+            print()
+
+
+#%%
